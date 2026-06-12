addfdc182d82b40a23960d6c4750973e3b540d15
Aluno <Aluno@DESKTOP-O31M9M7>
2026-06-12 10:19:57 -0300

    Adicionar arquivos de projeto.

---
+++ Jojos_Bizarre_Adventure/Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Jojos_Bizarre_Adventure
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 55;
+            this.listBox1.Items.AddRange(new object[] {
+            "STARDUST CRUSADERS",
+            "GOLDEN WIND",
+            "STONE OCEAN"});
+            this.listBox1.Location = new System.Drawing.Point(23, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(619, 169);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 233);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(349, 59);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(1068, 50);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Heróis";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(1068, 95);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Vilões";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form2";
+            this.Text = "Tela Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
---
+++ Jojos_Bizarre_Adventure/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jojos_Bizarre_Adventure
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Text == "STARDUST CRUSADERS" && radioButton1.Checked) {
+
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Jotaro Kujo");
+                comboBox1.Items.Add("Joseph Joestar");
+                comboBox1.Items.Add("Kakyoin");
+                comboBox1.Items.Add("Polnareff");
+                comboBox1.Items.Add("Avdol");
+                comboBox1.Items.Add("Iggy");
+            }
+           
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (listBox1.Text == "STARDUST CRUSADERS" && radioButton2.Checked)
+            {
+
+                comboBox1.Items.Clear();
+                comboBox1.Items.Add("Dio");
+                comboBox1.Items.Add("Midler");
+                comboBox1.Items.Add("Nena");
+                comboBox1.Items.Add("Hol Horse");
+                comboBox1.Items.Add("Steely Dan");
+                comboBox1.Items.Add("Forever");
+                comboBox1.Items.Add("ZZ");
+                comboBox1.Items.Add("Enya");
+                comboBox1.Items.Add("J. Geil");
+                comboBox1.Items.Add("Manish Boy");
+                comboBox1.Items.Add("Rubber Soul");
+                comboBox1.Items.Add("Devo");
+                comboBox1.Items.Add("Forever");
+                comboBox1.Items.Add("Forever");
+                comboBox1.Items.Add("Forever");
+                comboBox1.Items.Add("Forever");
+                comboBox1.Items.Add("Forever");
+                comboBox1.Items.Add("Forever");
+
+            }
+        }
+    }
+}
